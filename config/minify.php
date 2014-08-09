65adb362f5ca98a6d8c393543f1f99f0c17d1b19
KeJyun <kejyun@gmail.com>
2014-08-09 15:05:22 +0800

    add explain to config/minify.php

--- config/minify.php
+++ config/minify.php
@@ -1,8 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['assets_dir'] = 'assets';
+// output path where the compiled files will be stored
+$config['assets_dir'] = 'assets/';  
+
+// where to look for css files 
 $config['css_dir'] = 'assets/css';
+
+// where to look for js files 
 $config['js_dir'] = 'assets/js';
 
 // End of file minify.php
